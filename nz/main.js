72dc06fd1880172936cc2ce5bfac7100a26eb897
Johannes Köck <nepo@ydWLI-U1-32-11.uibk.ac.at>
2019-03-20 18:04:24 +0100

    Javaerstellt

---
+++ nz/main.js
@@ -0,0 +1,8 @@
+
+const div = document.getElementById("map");
+const lat = div.getAttribute("data-lat");
+const lng = div.getAttribute("data-lng");
+const title = div.getAttribute("data-title");
+
+
+console.log("breite:",lat,"länge:",lng,"Titel:",title);
