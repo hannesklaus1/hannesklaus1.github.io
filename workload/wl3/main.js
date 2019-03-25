8996f66ba4ee12e87be040750889d0e46bd56b4f
Johannes Köck <nepo@ydWLI-U1-38-154.uibk.ac.at>
2019-03-25 13:20:49 +0100

    wl3 Teil2 9/10 typeof operator

--- workload/wl3/main.js
+++ workload/wl3/main.js
@@ -1,3 +1,4 @@
-var myName= "Blasius";
-var myCity = "Noosa";
-console.log(`My Name is ${myName} and I live in ${myCity}`);
+let newVariable = 'Playing around with typeof.';
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
