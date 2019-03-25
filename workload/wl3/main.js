c1b1d830df7ed5d433dfc5981a13dc25b9122dac
Johannes Köck <nepo@ydWLI-U1-38-154.uibk.ac.at>
2019-03-25 12:47:54 +0100

    wl 3 5/10 Arithmetics

--- workload/wl3/main.js
+++ workload/wl3/main.js
@@ -1,4 +1,4 @@
-console.log("'Javascript'")
-console.log(2001)
-console.log('Woohoo! I love to code! #Codacademy')
-console.log(20.49)
+console.log(44+3.5)
+console.log(2019 - 1969)
+console.log(65/240)
+console.log(0.2708*100)
