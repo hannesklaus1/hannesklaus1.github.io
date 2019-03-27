9503c73418991bbd47d23c634a157dabe59b0049
Johannes Köck <nepo@ydWLI-U1-38-62.uibk.ac.at>
2019-03-27 16:35:32 +0100

    karte

--- adler/main.js
+++ adler/main.js
@@ -4,7 +4,8 @@ const breite = div.getAttribute("data-lat");
 const laenge = div.getAttribute("data-lng");
 const breite2 = div.gettAttribute("data2-lat");
 const laenge2 = div.gettAttribute("data2-lng");
-const titel = div.getAttribute("data-title");
+const titel = div.getAttribute("data-title1");
+const titel2 =div.getAttribute("data-title2");
 
 console.log("breite:",breite,"länge:",laenge,"Titel:",titel);
 
@@ -32,4 +33,5 @@ let Ende = L.marker(
 ).addTo(karte);
 
 //popup an Pin setzten
-pin.bindPopup(titel).openPopup();
+Start.bindPopup(titel).openPopup();
+Ende.bindPopup(titel2).openPopup();
