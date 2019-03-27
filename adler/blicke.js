646cdbc816a04057845b401b87f5954ee86d5936
Johannes Köck <nepo@ydWLI-U1-38-62.uibk.ac.at>
2019-03-27 18:19:45 +0100

    neues js file in dem die Objekte gespeichert  sind

---
+++ adler/blicke.js
@@ -0,0 +1,23 @@
+const adlerblicke = [ // ein array erstellen damit man mit schleife druchgehen kann
+  { // ein Objekt "blick1" erstellt mit div werten
+    kunde: " Wilder Kaiser ",
+    standort: "Gruttenhütte",
+    seehoehe: "1640",
+    lat: "47.55564",
+    lng: "12.31861",
+  },
+  { // ein Objekt "blick1" erstellt mit div werten
+    kunde: " Bergbahnen Scheffau",
+    standort: "Brandstadl",
+    seehoehe: "1640",
+    lat: "47.491200",
+    lng: "12.24805556",
+  },
+  {
+    kunde: " Lechtal Tourismus ",
+    standort: "Sonnalm Jöchelspitze",
+    seehoehe: "1786",
+    lat: "47.275325",
+    lng: "10.364524",
+  }
+];
--- adler/main.js
+++ adler/main.js
@@ -37,54 +37,6 @@ let Ende = L.marker(
 Start.bindPopup(titel).openPopup();
 Ende.bindPopup(titel2).openPopup();
 
-const blick1 = { // ein Objekt "blick1" erstellt mit div werten
-  kunde: " Wilder Kaiser ",
-  standort: "Gruttenhütte",
-  seehoehe: "1640",
-  lat: "47.55564",
-  lng: "12.31861",
-};
-
-const blick2 = { // ein Objekt "blick1" erstellt mit div werten
-  kunde: " Bergbahnen Scheffau",
-  standort: "Brandstadl",
-  seehoehe: "1640",
-  lat: "47.491200",
-  lng: "12.24805556",
-};
-
-const blick3 = { // ein Objekt "blick1" erstellt mit div werten
-  kunde: " Lechtal Tourismus ",
-  standort: "Sonnalm Jöchelspitze",
-  seehoehe: "1786",
-  lat: "47.275325",
-  lng: "10.364524",
-};
-
-
-const adlerblicke = [ // ein array erstellen damit man mit schleife druchgehen kann
-  { // ein Objekt "blick1" erstellt mit div werten
-    kunde: " Wilder Kaiser ",
-    standort: "Gruttenhütte",
-    seehoehe: "1640",
-    lat: "47.55564",
-    lng: "12.31861",
-  },
-  { // ein Objekt "blick1" erstellt mit div werten
-    kunde: " Bergbahnen Scheffau",
-    standort: "Brandstadl",
-    seehoehe: "1640",
-    lat: "47.491200",
-    lng: "12.24805556",
-  },
-  {
-    kunde: " Lechtal Tourismus ",
-    standort: "Sonnalm Jöchelspitze",
-    seehoehe: "1786",
-    lat: "47.275325",
-    lng: "10.364524",
-  }
-];
 
 for (let blick of adlerblicke) {
   let blickpin = L.marker(
