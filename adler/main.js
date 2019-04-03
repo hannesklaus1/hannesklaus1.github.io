e637824dbea4bd58ba9bfa62a7c17470c2fce601
Johannes Köck <nepo@ydWLI-U1-39-92.uibk.ac.at>
2019-04-03 18:18:05 +0200

    coordinaten Plugin LOKAL

--- adler/main.js
+++ adler/main.js
@@ -111,3 +111,5 @@ for (let blick of adlerblicke) {
 karte.fitBounds(markerGruppe.getBounds());
 karte.addControl(new L.Control.Fullscreen());
 var hash = new L.Hash(karte);
+var coords = new L.Control.Coordinates();
+coords.addTo(karte);
