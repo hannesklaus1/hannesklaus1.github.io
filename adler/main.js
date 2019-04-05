24909d282267a28616bba0a8c68725d6353c385b
Johannes Köck <nepo@Nepos-Air.local>
2019-04-05 11:30:03 +0200

    edit

--- adler/main.js
+++ adler/main.js
@@ -24,7 +24,7 @@ const kartenLayer = {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
   }),
   geolandbasemap1: L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
-    subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"],
+    subdomains: ["maps", "maps1", "maps2", "maps3", "maps4"], 
     attribution: 'Datenquelle: <a href="://https:basemap.at“> basemap.at </a>'
   }),
   geolandbasemap_overlay: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
