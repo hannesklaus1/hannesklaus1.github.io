67ae6d307a81fd2502220c2018adb62f4c9ef43b
Johannes Köck <nepo@ydWLI-U1-38-220.uibk.ac.at>
2019-04-10 16:41:19 +0200

    Karte von IBK erstellt, mit standortermittlung

--- ibk/main.js
+++ ibk/main.js
@@ -1,22 +1,4 @@
-const div = document.getElementById("map");
-const breite = div.getAttribute("data-lat");
-const laenge = div.getAttribute("data-lng");
-const breite2 = div.getAttribute("data2-lat");
-const laenge2 = div.getAttribute("data2-lng");
-const titel = div.getAttribute("data-titel");
-const titel2 = div.getAttribute("data-titel2");
-
-console.log("breite:", breite, "länge:", laenge, "Titel:", titel);
-
-// initialieren der Karte
-
 let karte = L.map("map");
-//console.log(karte);
-
-// auf Ausschnitt zoomen
-//kann duch Markergruppe und fit to bounds automatisch gerufen werden
-
-//Openstreetmap einbinden
 
 const kartenLayer = {
   osm: L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
@@ -62,57 +44,38 @@ const kartenLayer = {
   stamen_watercolor: L.tileLayer("https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg", {
     subdomains: ["a", "b", "c"],
     attribution: 'Map tiles by Stamen Design, under CC BY 3.0. Data by OpenStreetMap, under CC BY SA.'
-  })
+  }),
 };
-//Einen Kartenlayer laden
+
 kartenLayer.osm.addTo(karte);
 
-//auswahlmenü hinzufügen für die Layer
-L.control.layers({
-  "Geoland Basemap Map1": kartenLayer.geolandbasemap1,
-  "Geoland Basemap Overlay": kartenLayer.geolandbasemap_overlay,
-  "Geoland Basemap Grau": kartenLayer.geolandbasemap_grau,
-  "Geoland Basemap HDPI": kartenLayer.geolandbasemap_hdpi,
-  "Geoland Basemap Orthofoto": kartenLayer.geolandbasemap_ortho,
-  "Geoland Basemap Gelaende": kartenLayer.geolandbasemap_gelande,
-  "Geoland Basemap Oberflaeche": kartenLayer.geolandbasemap_oberfl,
-  "STAMEN Toner": kartenLayer.stamen_toner,
-  "STAMEN relief": kartenLayer.stamen_relief,
-  "STAMEN watercolor": kartenLayer.stamen_watercolor,
-}).addTo(karte)
+  L.control.layers({
+    "Geoland Basemap Map1": kartenLayer.geolandbasemap1,
+    "Geoland Basemap Overlay": kartenLayer.geolandbasemap_overlay,
+    "Geoland Basemap Grau": kartenLayer.geolandbasemap_grau,
+    "Geoland Basemap HDPI": kartenLayer.geolandbasemap_hdpi,
+    "Geoland Basemap Orthofoto": kartenLayer.geolandbasemap_ortho,
+    "Geoland Basemap Gelaende": kartenLayer.geolandbasemap_gelande,
+    "Geoland Basemap Oberflaeche": kartenLayer.geolandbasemap_oberfl,
+    "STAMEN Toner": kartenLayer.stamen_toner,
+    "STAMEN relief": kartenLayer.stamen_relief,
+    "STAMEN watercolor": kartenLayer.stamen_watercolor,
+  }).addTo(karte);
 
+///karte.setView(
+  //[47,11],13
+//)
 
-//Positionsmarker Hinzufügen
-let Start = L.marker(
-  [breite, laenge]
-).addTo(karte);
+karte.locate({
+  setView: true,
+  maxZoom: 18
+});
 
-let Ende = L.marker(
-  [breite2, laenge2]
-).addTo(karte);
-
-//popup an Pin setzten
-Start.bindPopup(titel).openPopup();
-Ende.bindPopup(titel2).openPopup();
-
-let markerGruppe = L.featureGroup().addTo(karte);
-
-for (let blick of adlerblicke) {
-  let blickpin = L.marker(
-    [blick.lat, blick.lng]
-  ).addTo(markerGruppe);
-  blickpin.bindPopup(
-    `<h1> Standort ${blick.standort} </h1>
-    <p> Höhe ${blick.seehoehe} m</p>
-    <em> Kunde: ${blick.kunde}</em>`
-  )
-};
-
-karte.fitBounds(markerGruppe.getBounds());
-karte.addControl(new L.Control.Fullscreen());
-var hash = new L.Hash(karte);
-var coords = new L.Control.Coordinates();
-coords.addTo(karte);
-karte.on('click', function(e) {
-	coords.setCoordinates(e);
+karte.on("locationfound", function(event){
+  console.log(event);
+  L.marker([
+    event.latitude, event.longitude
+  ]).addTo(karte);
+  L.circle([event.latitude, event.longitudex],
+  {radius: event.accuracy/2}).addTo(karte)
 });
